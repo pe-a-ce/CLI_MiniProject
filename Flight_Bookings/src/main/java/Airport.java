1adbadb20c9843fafad3c86fbd2755bf68f0e8d5
James Stillman <jamesstillman99@gmail.com>
2022-05-04 16:32:29 +0100

    Classes implemented

---
+++ Flight_Bookings/src/main/java/Airport.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Airport {
+
+    private List<Flight> allFlights;
+
+    //constructor
+    public Airport() {
+        this.allFlights = new ArrayList<Flight>();
+    }
+
+    //behaviours
+
+    public List<Flight> getAllFlights() {
+        return allFlights;
+    }
+
+    public void displayFlights() {
+        System.out.println(allFlights.toString());
+    }
+
+    public void addPassengerToFlight(Passenger passenger, Flight flight) {
+        flight.addPassenger(passenger);
+    }
+
+    public void addFlight(Destination destination, int flightId) {
+        allFlights.add(new Flight (destination, flightId));
+
+    }
+
+}
---
+++ Flight_Bookings/src/main/java/Flight.java
@@ -0,0 +1,61 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Flight {
+
+
+    //Properties of flight class
+
+    private Destination destination;
+    private int flightId;
+    private List<Passenger> passengers;
+
+
+    //Getters & Setters
+
+    public Destination getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Destination destination) {
+        this.destination = destination;
+    }
+
+    public int getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(int flightId) {
+        this.flightId = flightId;
+    }
+
+    public List<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(List<Passenger> passengers) {
+        this.passengers = passengers;
+    }
+
+
+    //Constructors
+
+    public Flight(Destination destination, int flightId) {
+        this.destination = destination;
+        this.flightId = flightId;
+        this.passengers = new ArrayList<>();
+    }
+
+
+    //Methods
+
+    public void addPassenger(Passenger passenger){
+        this.passengers.add(passenger);
+    }
+
+    public void removePassenger(Passenger passenger){
+        this.passengers.remove(passenger);
+    }
+
+}
